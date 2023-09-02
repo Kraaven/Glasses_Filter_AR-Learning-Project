af4745516827edb8280120c05182900d69872cf9
Kraaven <dhruv.bhojaraj@gmail.com>
2023-09-02 13:00:01 +0530

    Colors and Persistence
    -Added Colors
    -Added Working buttons
    -Added automatic save of the current face filter and color

---
+++ Assets/Scripts/StartandSave.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = Unity.Mathematics.Random;
+
+
+public class StartandSave : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject parentCanvas;
+
+    void Start()
+    {
+        parentCanvas = GameObject.FindGameObjectWithTag("ButtonController");
+        parentCanvas.GetComponent<buttonsystem>().LoadStart();
+    }
+}
--- Assets/Scripts/buttonsystem.cs
+++ Assets/Scripts/buttonsystem.cs
@@ -1,10 +1,93 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class buttonsystem : MonoBehaviour
-{ 
+{
+    public int face;
+    public int color;
+    
+    public GameObject currentfilter;
+    public Material currentColor;
+    public Material Red;
+    public Material Green;
+    public Material Blue;
+    
+    public bool ini = false;
+    public void LoadStart()
+    {
+        if (ini == false)
+        {
+            int temp = Random.Range(0, 3);
+
+            switch (temp)
+            {
+                case 0:
+                    ShowCat();
+                    break;
+                
+                case 1:
+                    ShowMask();
+                    break;
+                case 2:
+                    ShowMono();
+                    break;
+            }
+
+            temp = Random.Range(0, 3);
+            switch (temp)
+            {
+                case 0:
+                    setgreen();
+                    break;
+                
+                case 1:
+                    setblue();
+                    break;
+                case 2:
+                    Setred();
+                    break;
+            }
+
+            ini = true;
+        }
+        else
+        {
+            switch (face)
+            {
+                case 0:
+                    ShowCat();
+                    break;
+                
+                case 1:
+                    ShowMask();
+                    break;
+                case 2:
+                    ShowMono();
+                    break;
+            }
+
+            switch (color)
+            {
+                case 0:
+                    setgreen();
+                    break;
+                
+                case 1:
+                    setblue();
+                    break;
+                case 2:
+                    Setred();
+                    break;
+            }
+        }
+
+        
+    }
+    
     
 
     public void ShowMask()
@@ -12,6 +95,9 @@ public class buttonsystem : MonoBehaviour
         GameObject.FindGameObjectWithTag("Cat").GetComponent<MeshRenderer>().enabled = false;
         GameObject.FindGameObjectWithTag("Monocle").GetComponent<MeshRenderer>().enabled = false;
         GameObject.FindGameObjectWithTag("Mask").GetComponent<MeshRenderer>().enabled = true;
+        currentfilter = GameObject.FindGameObjectWithTag("Mask");
+        face = 1;
+        currentfilter.GetComponent<Renderer>().material = currentColor;
     }
     
     public void ShowCat()
@@ -19,6 +105,9 @@ public class buttonsystem : MonoBehaviour
         GameObject.FindGameObjectWithTag("Monocle").GetComponent<MeshRenderer>().enabled = false;
         GameObject.FindGameObjectWithTag("Mask").GetComponent<MeshRenderer>().enabled = false;
         GameObject.FindGameObjectWithTag("Cat").GetComponent<MeshRenderer>().enabled = true;
+        currentfilter = GameObject.FindGameObjectWithTag("Cat");
+        face = 0;
+        currentfilter.GetComponent<Renderer>().material = currentColor;
     }
     
     public void ShowMono()
@@ -26,5 +115,47 @@ public class buttonsystem : MonoBehaviour
         GameObject.FindGameObjectWithTag("Cat").GetComponent<MeshRenderer>().enabled = false;
         GameObject.FindGameObjectWithTag("Mask").GetComponent<MeshRenderer>().enabled = false;
         GameObject.FindGameObjectWithTag("Monocle").GetComponent<MeshRenderer>().enabled = true;
+        currentfilter = GameObject.FindGameObjectWithTag("Monocle");
+        face = 2;
+        currentfilter.GetComponent<Renderer>().material = currentColor;
+    }
+
+    public GameObject getcurrent()
+    {
+        if (GameObject.FindGameObjectWithTag("Cat").GetComponent<MeshRenderer>().enabled == true)
+        {
+            return GameObject.FindGameObjectWithTag("Cat");
+        }
+        if(GameObject.FindGameObjectWithTag("Mask").GetComponent<MeshRenderer>().enabled == true)
+        {
+            return GameObject.FindGameObjectWithTag("Mask");
+        }
+        if(GameObject.FindGameObjectWithTag("Monocle").GetComponent<MeshRenderer>().enabled == true)
+        {
+            return GameObject.FindGameObjectWithTag("Monocle");
+        }
+
+        return GameObject.FindGameObjectWithTag("Monocle");
+    }
+
+    public void Setred()
+    {
+        getcurrent().GetComponent<Renderer>().material = Red;
+        currentColor = Red;
+        color = 2;
+    }
+
+    public void setblue()
+    {
+        getcurrent().GetComponent<Renderer>().material = Blue;
+        currentColor = Blue;
+        color = 1;
+    }
+
+    public void setgreen()
+    {
+        getcurrent().GetComponent<Renderer>().material = Green;
+        currentColor = Green;
+        color = 0;
     }
 }
